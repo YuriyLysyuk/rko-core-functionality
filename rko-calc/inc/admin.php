4dc8be82714bfb297dc49cb50f5c687acc3c729f
Yuriy Lysyuk <lysyuk.y@gmail.com>
2020-02-18 11:13:20 +0300

    Изменил вывод в админке значений колонки Банк на имя записи вместо мета

--- rko-calc/inc/admin.php
+++ rko-calc/inc/admin.php
@@ -30,7 +30,7 @@ add_filter('manage_tariffs_posts_columns', 'add_acf_columns_to_tariffs_list', 20
 function tariffs_custom_column ( $column, $post_id ) {
   switch ( $column ) {
     case 'bank':
-      echo get_post_meta ( get_post_meta ( $post_id, 'bank', true ), 'name_chto', true );
+      echo esc_html(get_the_title(get_post_meta($post_id, 'bank', true)));
       break;
   }
 }
