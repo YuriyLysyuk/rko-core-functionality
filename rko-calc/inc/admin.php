4b583ecb36d6d430d0a1542622e2e3f687ef12d0
Yuriy Lysyuk <lysyuk.y@gmail.com>
2020-02-18 11:14:26 +0300

    Отформатировал файл с помощью Prettier+

--- rko-calc/inc/admin.php
+++ rko-calc/inc/admin.php
@@ -21,17 +21,27 @@ function add_acf_columns_to_tariffs_list($columns)
   return $columns + $tariffColumns;
 }
 
-add_filter('manage_tariffs_posts_columns', 'add_acf_columns_to_tariffs_list', 20);
+add_filter(
+  'manage_tariffs_posts_columns',
+  'add_acf_columns_to_tariffs_list',
+  20
+);
 
 /**
  * Заполняем колонки в списке тарифов
  *
  */
-function tariffs_custom_column ( $column, $post_id ) {
-  switch ( $column ) {
+function tariffs_custom_column($column, $post_id)
+{
+  switch ($column) {
     case 'bank':
       echo esc_html(get_the_title(get_post_meta($post_id, 'bank', true)));
       break;
   }
 }
-add_action ( 'manage_tariffs_posts_custom_column', 'tariffs_custom_column', 10, 2 );
+add_action(
+  'manage_tariffs_posts_custom_column',
+  'tariffs_custom_column',
+  10,
+  2
+);
