a985ebac7bc3ad3760cc76243d3df867bd508b47
Yuriy Lysyuk <lysyuk.y@gmail.com>
2020-03-18 14:34:16 +0300

    Добавил в шорткод заготовку формы

--- rko-calc/inc/shortcode.php
+++ rko-calc/inc/shortcode.php
@@ -11,8 +11,19 @@
  * Добавляем шорткод для вывода калькулятора на странице
  *
  */
-function rko_calc_shortcode($atts)
+function rko_calc_shortcode()
 {
-	return rko_calc();
+  $form = '
+    <form id="rko-calc-form">
+			<input type="text" name="income" id="income" value="0">
+      <input type="submit" value="Получить личный ТОП банков" id="submit">
+    </form>
+    <div id="rko-calc-results">
+		
+    </div>
+  ';
+
+  // return $form;
+  rko_calc(1);
 }
 add_shortcode('rko_calc', 'rko_calc_shortcode');
