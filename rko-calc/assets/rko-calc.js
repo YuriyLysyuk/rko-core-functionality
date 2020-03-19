8a1fcd8bb5459d0be2a7a5862c480b5626cecb25
Yuriy Lysyuk <lysyuk.y@gmail.com>
2020-03-19 12:34:22 +0300

    Добавил передачу полей формы в REST API

--- rko-calc/assets/rko-calc.js
+++ rko-calc/assets/rko-calc.js
@@ -1,30 +1,26 @@
 (function($) {
-  "use strict";
+  'use strict';
 
-  $.ajax({
-    type: "GET",
-    url: rkoCalc.restURL + "rko-calc/v1/calculate",
-
-    success: function(post) {
-      console.clear;
-      console.log(post);
-    }
-  });
-
-  let rkoCalcForm = $("#rko-calc-form");
-  rkoCalcForm.on("submit", function(e) {
+  let rkoCalcForm = $('#rko-calc-form');
+  rkoCalcForm.on('submit', function(e) {
     e.preventDefault();
 
-    $.ajax({
-      type: "GET",
-      url: rkoCalc.restURL + "rko-calc/v1/calculate",
-      data: rkoCalcForm.serialize(),
+    // serialize() не обрабатывает поля без name, значит служебные переключатели можно исключить из URL
+    let rkoCalcFormData = rkoCalcForm.serialize();
+    console.clear();
+    console.log(rkoCalcFormData);
+    console.log('REST URL: ' + rkoCalc.restURL + 'rko-calc/v1/calculate?' + rkoCalcFormData);
 
+    $.ajax({
+      type: 'GET',
+      url: rkoCalc.restURL + 'rko-calc/v1/calculate?' + rkoCalcFormData,
+      data: rkoCalcFormData,
+      
       success: function(post) {
-        console.clear;
         console.log(post);
       }
     });
+    
   });
 
   /* function results(val) {
