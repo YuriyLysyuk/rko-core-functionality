051c4f2b2e9b293bbc36b59f68b5842c9ba75fc0
Yury Lysyuk <lysyuk.y@gmail.com>
2020-05-16 16:05:15 +0300

    добавил шаблон формирования html с результатом по отдельному тарифу

--- rko-calc/assets/rko-calc.js
+++ rko-calc/assets/rko-calc.js
@@ -351,6 +351,49 @@
     });
 
 
+    // Шаблон формирования html с результатом по отдельному тарифу
+    function tariffTemplate(rkoCalcResult, index) {
+      // Получаем пользовательские данные, используемые в расчетах
+      let userParams = rkoCalcResult.user_params;
+      // Получаем параметры текущего тарифа
+      let tariffOptions = rkoCalc.allTariffOptions[rkoCalcResult.id];
+
+      // Возвращаем html с версткой по отдельному тарифу
+      // tariffDetails() возвращает верстку с детальной расшифровкой результата
+      return `
+        <li class="result-wrap">
+          <div class="result-position">${index + 1}</div>
+          <div class="result-bank">
+            <img src="/biglogo_326.svg" class="result-bank-logo">
+            <div class="result-bаnk-license">лиц. № 1234</div>
+          </div>
+          <div class="result-tariff">
+            <div class="result-tariff-label">Тариф</div>
+            <div class="result-tariff-name">${tariffOptions.name}</div>
+          </div>
+          <div class="result-calculated">
+            <div class="result-calculated-sum price">${moneyFormatWOS.to(
+              rkoCalcResult.calculated_sum
+            )} <span>₽/мес</span></div>
+            <div class="result-calculated-details">
+              <span>Детально</span><span> <svg class="open" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m11.9932649 19.500812c-.6352342.000819-1.2400333-.2833911-1.65984-.78l-9.41856003-11.15199999c-.61913211-.76859096-.53599584-1.91147321.1872475-2.57410938.72324334-.66263618 1.82204752-.60264878 2.4748325.13510938l8.23584003 9.75199999c.0455704.0541548.1113462.0852092.18048.0852092.0691337 0 .1349096-.0310544.18048-.0852092l8.23584-9.75199999c.4135143-.51333692 1.0615094-.75310007 1.6932379-.626511s1.1480791.59966995 1.3492454 1.2361798c.2011663.63650986.0555875 1.33658731-.3804033 1.8293312l-9.41568 11.14799999c-.4205256.497811-1.0261445.7833703-1.66272.784z" fill-rule="evenodd"/></svg><svg class="close" enable-background="new 0 0 24 24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m12 4.5c.6 0 1.2.3 1.7.8l9.4 11.2c.6.8.5 1.9-.2 2.6s-1.8.6-2.5-.1l-8.2-9.8c0-.2-.1-.2-.2-.2s-.1 0-.2.1l-8.2 9.8c-.4.5-1.1.8-1.7.6-.6-.1-1.1-.6-1.3-1.2s-.1-1.3.4-1.8l9.4-11.1c.4-.6 1-.9 1.6-.9z"/></svg></span>
+            </div>
+          </div>
+          <div class="result-button-wrap">
+            <a href="${
+              tariffOptions.button.url
+            }" target="_blank" onclick="${tariffOptions.button.onclick.calc_list}" class="result-button button button-small button-outline">Перейти на сайт</a>
+          </div>
+          <div class="result-details-wrap">
+            ${tariffDetails(
+              rkoCalcResult.calculated,
+              tariffOptions,
+              userParams
+            )}
+          </div>
+        </li>
+      `;
+    }
 
     // Функция для вывода результатов работы калькулятора
     // Для каждого тарифа вызывает шаблон формирования html с результатом
