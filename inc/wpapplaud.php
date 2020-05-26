795c2844bb4de611a7dc5ae1a82a0f175a747ea7
Yury Lysyuk <lysyuk.y@gmail.com>
2020-05-26 18:18:42 +0300

    Перепутал с прошлым, это форматирование файла WPApplaud. А прошлое: добавил проверку при наличии шоткода с калькулятором, использовать в ссылках шаринга url c параметрами

--- inc/wpapplaud.php
+++ inc/wpapplaud.php
@@ -5,24 +5,26 @@
  * @package      RKOCoreFunctionality
  * @author       Yuriy Lysyuk
  * @since        1.1.0
-**/
+ **/
 
-/** 
+/**
  * Добавляем функционал аплодиментов в мета данные после статьи
- * 
+ *
  */
-function rkocf_wpapplaud() {
-	if ( function_exists( 'wp_applaud' ) ) {
-		global $post;
+function rkocf_wpapplaud()
+{
+  if (function_exists('wp_applaud')) {
+    global $post;
 
-		// Скрываем вывод аплодисментов, если он отключен в админке
-		$value = get_post_meta( $post->ID, '_wp_applaud_exclude', true );
-		if($value) return;
+    // Скрываем вывод аплодисментов, если он отключен в админке
+    $applaudExclude = get_post_meta($post->ID, '_wp_applaud_exclude', true);
+    if ($applaudExclude) {
+      return;
+    }
 
-		echo '<span class="wp-applaud-wrap">';
-		wp_applaud();
-		echo '</span>';
-
-	}
+    echo '<span class="wp-applaud-wrap">';
+    wp_applaud();
+    echo '</span>';
+  }
 }
-add_action( 'genesis_entry_footer', 'rkocf_wpapplaud' );
+add_action('genesis_entry_footer', 'rkocf_wpapplaud');
