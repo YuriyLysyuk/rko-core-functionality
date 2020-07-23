ad08e64ba053ebd715702bf93daf9cfd2d46f1d1
Yury Lysyuk <lysyuk.y@gmail.com>
2020-07-23 10:53:43 +0300

    Защитил исходник скрипта rko-calc числовым кодом, который убирается при минификации скрипта

--- gulpfile.js
+++ gulpfile.js
@@ -4,20 +4,20 @@ const gulp = require("gulp"),
   terser = require("gulp-terser");
 gulp.task("rko-scripts", function () {
   return gulp
-    .src([
-      "rko-calc/assets/js/src/rko-calc.js",
-    ])
+    .src(["rko-calc/assets/js/src/*.js"])
     .pipe(sourcemaps.init()) // Начинаем запись карты
     .pipe(terser()) // Сжимаем JS файл
+    .pipe(
+      rename(function (path) {
+        path.basename = path.basename.replace(/-\d+/g, "");
+      })
+    ) // Убираем числовой код из файла
     .pipe(rename({ suffix: ".min" })) // Добавляем .min
     .pipe(sourcemaps.write("./")) // Заканчиваем запись карты
     .pipe(gulp.dest("rko-calc/assets/js")); // Выгружаем в папку
 });
 
 gulp.task("watch", function () {
-  gulp.watch(
-    ["rko-calc/assets/js/src/rko-calc.js"],
-    gulp.parallel("rko-scripts")
-  );
+  gulp.watch(["rko-calc/assets/js/src/*.js"], gulp.parallel("rko-scripts"));
 });
 gulp.task("default", gulp.parallel("rko-scripts", "watch"));
